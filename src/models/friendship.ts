049c465609fe1d6ab5b8827e2e8974df6ff96fc5
MasterMobo <mastermobo101@gmail.com>
2024-08-09 16:50:57 +0700

    added data models

---
+++ src/models/friendship.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "./user";
+
+interface IFriendship {
+    sender: IUser;
+    receiver: IUser;
+    status: string;
+}
+
+const friendshipSchema = new Schema<IFriendship>({
+    sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    status: {
+        type: String,
+        required: false,
+        default: "pending",
+        enum: ["pending", "accepted"],
+    },
+});
+
+const Friendship = model<IFriendship>("Friendship", friendshipSchema);
+
+export { Friendship, IFriendship };
---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+interface IUser {
+    fullName: string;
+    email: string;
+    password: string;
+    profilePic: string;
+}
+
+const userSchema = new Schema<IUser>({
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, required: true },
+});
+
+const User = model<IUser>("User", userSchema);
+
+export { User, IUser };
