29a8b0d4d9f2d980ea7ada97550c453bb39e5b5c
phuchauxd12 <118866081+phuchauxd12@users.noreply.github.com>
2024-08-11 23:50:00 +0700

    feat: get friends

--- src/controllers/friend/getFriendRequests.ts
+++ src/controllers/friend/getFriendRequests.ts
@@ -1,29 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { Friendship } from "../../models/friendship";
 import { User } from "../../models/user";
-
 const getFriends = async (req: Request, res: Response) => {
 
     const {userID} = req.body;
     const friendships = await Friendship.find({
-        $or: [
-            { sender: userID },
-            { receiver: userID }
-        ],
-        status: "accepted"
+        receiver: userID,
+        status: "pending"
     });
 
-    const friendIDs = friendships.map(friendship =>
-        friendship.sender.toString() === userID
-            ? friendship.receiver
-            : friendship.sender
-    );
-    
-     const friendList = await User.find({
+    const friendIDs = friendships.map(friendship => friendship.sender);
+     const requestList = await User.find({
             _id: { $in: friendIDs }
         });
     
-        return res.json(friendList);
+        return res.json(requestList);
 };
 
 export default getFriends;
