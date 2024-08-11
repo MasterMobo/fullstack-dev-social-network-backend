b2bd4810777bff835a6e9e25a31ff1693c647155
Alakinnn <s3978452@rmit.edu.vn>
2024-08-11 22:18:56 +0700

    fix: accept request logic

--- src/controllers/friendship/acceptFriendRequest.ts
+++ src/controllers/friendship/acceptFriendRequest.ts
@@ -1,25 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import { Friendship } from "../../models/friendship";
-import { NotFoundError } from "../../errors";
+import { BadRequestError } from "../../errors";
 
 const acceptFriendRequest = async (req: Request, res: Response, next: NextFunction) => {
-    const { senderID, receiverID, status = "accepted" } = req.body;
+    const { senderID, receiverID } = req.body;
 
     const friendship = await Friendship.findOne({
         sender: senderID,
         receiver: receiverID,
-        status: "pending",
     });
 
     if (!friendship) {
-        next(new NotFoundError("Friendship request not found!"));
+        return next(new BadRequestError("Invalid sender or receiver"));
     }
 
-    friendship!.status = status;
+    friendship.status = "accepted";
 
     await friendship!.save();
 
-    res.json(friendship);
+    return res.json(friendship);
 };
 
 export default acceptFriendRequest
