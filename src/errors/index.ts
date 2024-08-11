e1b8757d525b9fbbae6d235c9dd0f30da0495c2a
Alakinnn <s3978452@rmit.edu.vn>
2024-08-11 17:56:53 +0700

    chore: update import

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,6 @@
 import APIError from "./APIError";
 import NotFoundError from "./NotFoundError";
 import UnauthorizedError from "./UnauthorizedError";
+import BadRequestError from "./BadRequestError"
 
-export { APIError, NotFoundError, UnauthorizedError };
+export { APIError, NotFoundError, UnauthorizedError, BadRequestError };
