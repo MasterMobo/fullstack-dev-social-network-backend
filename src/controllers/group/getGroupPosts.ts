6b3c932fe1f91c431c514335bc5bb015b99760b0
MasterMobo <mastermobo101@gmail.com>
2024-08-24 14:06:42 +0700

    added private checking for get group posts

--- src/controllers/group/getGroupPosts.ts
+++ src/controllers/group/getGroupPosts.ts
@@ -1,9 +1,35 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Post } from "../../models/post";
+import { Group } from "../../models/group";
+import { NotFoundError, UnauthorizedError } from "../../errors";
 
-const getGroupPosts = async (req: Request, res: Response) => {
+const getGroupPosts = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     // Get group ID from request parameters
     const { groupId } = req.params;
+
+    // Check if group exists
+    const group = await Group.findById(groupId).exec();
+    if (!group) {
+        return next(new NotFoundError("Group not found"));
+    }
+
+    if (group.visibility === "private") {
+        // Check if current user is a member of the group
+        const user = req.signedCookies["user"];
+        const isMember = group.members.find((member) =>
+            member._id.equals(user._id)
+        );
+
+        if (!isMember) {
+            return next(
+                new UnauthorizedError("You are not a member of this group")
+            );
+        }
+    }
 
     // Find all posts in the group
     const posts = await Post.find({ groupID: groupId }).exec();
