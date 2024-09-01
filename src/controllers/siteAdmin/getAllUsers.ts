93c141f896d31896fb36ca221f13d74043af218b
MasterMobo <mastermobo101@gmail.com>
2024-09-01 15:09:21 +0700

    added admin status query

--- src/controllers/siteAdmin/getAllUsers.ts
+++ src/controllers/siteAdmin/getAllUsers.ts
@@ -1,15 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { Admin, IUser, User } from "../../models/user";
-import { UnauthorizedError } from "../../errors";
+import { BadRequestError, UnauthorizedError } from "../../errors";
 
 const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     const admin: IUser = req.signedCookies["user"];
+    const { status } = req.query;
+
+    if (status && status !== "active" && status !== "suspended") {
+        return next(new BadRequestError("Invalid status!"));
+    }
 
     if (!(await Admin.findById(admin._id))) {
         return next(new UnauthorizedError("User not authorized!"));
     }
 
-    const users = await User.find().exec();
+    let users: IUser[];
+
+    if (status) {
+        users = await User.find({ status: status }).exec();
+    } else {
+        users = await User.find().exec();
+    }
+
     res.status(200).json({ users });
 };
 
