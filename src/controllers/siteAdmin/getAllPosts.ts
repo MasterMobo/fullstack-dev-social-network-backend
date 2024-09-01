4578da89c7cf6e7c0944f3c9eaa4368569aabf8c
MasterMobo <mastermobo101@gmail.com>
2024-09-01 10:47:07 +0700

    fixed suspend user bug

--- src/controllers/siteAdmin/getAllPosts.ts
+++ src/controllers/siteAdmin/getAllPosts.ts
@@ -4,14 +4,14 @@ import { UnauthorizedError } from "../../errors";
 import { Post } from "../../models/post";
 
 const getAllPosts = async (req: Request, res: Response, next: NextFunction) => {
-  const admin: IUser = req.signedCookies["user"];
+    const admin: IUser = req.signedCookies["user"];
 
-  if (!(await Admin.findById(admin._id))) {
-    return next(new UnauthorizedError("User not authorized!"));
-  }
+    if (!(await Admin.findById(admin._id))) {
+        return next(new UnauthorizedError("User not authorized!"));
+    }
 
-  const posts = await Post.find().populate("user").populate("group").exec();
-  res.status(200).json(posts);
+    const posts = await Post.find().populate("user").populate("group").exec();
+    res.status(200).json({ posts });
 };
 
 export default getAllPosts;
--- src/controllers/siteAdmin/suspendUser.ts
+++ src/controllers/siteAdmin/suspendUser.ts
@@ -1,42 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import { Admin, IUser, User } from "../../models/user";
 import {
-  BadRequestError,
-  NotFoundError,
-  UnauthorizedError,
+    BadRequestError,
+    NotFoundError,
+    UnauthorizedError,
 } from "../../errors";
 import { Types } from "mongoose";
 
 const suspendUser = async (req: Request, res: Response, next: NextFunction) => {
-  const admin: IUser = req.signedCookies["user"];
+    const admin: IUser = req.signedCookies["user"];
 
-  if (!(await Admin.findById(admin._id))) {
-    return next(new UnauthorizedError("User not authorized!"));
-  }
+    if (!(await Admin.findById(admin._id))) {
+        return next(new UnauthorizedError("User not authorized!"));
+    }
 
-  const { userId } = req.params;
+    const { userId } = req.params;
 
-  if (!Types.ObjectId.isValid(userId)) {
-    return next(new BadRequestError("Invalid user id"));
-  }
+    if (!Types.ObjectId.isValid(userId)) {
+        return next(new BadRequestError("Invalid user id"));
+    }
 
-  const userToSuspend = await User.findById(userId).exec();
-  if (!userToSuspend) {
-    return next(new NotFoundError("User not found!"));
-  }
+    const userToSuspend = await User.findById(userId).exec();
+    if (!userToSuspend) {
+        return next(new NotFoundError("User not found!"));
+    }
 
-  const isSuspendingAdmin = await Admin.findById(userToSuspend._id).exec();
-  if (isSuspendingAdmin) {
-    return next(new BadRequestError("Cannot suspend another site admin"));
-  }
+    const isSuspendingAdmin = await Admin.findById(userToSuspend._id).exec();
+    if (isSuspendingAdmin) {
+        return next(new BadRequestError("Cannot suspend another site admin"));
+    }
 
-  if (userToSuspend.status === "suspended") {
-    return next(new BadRequestError("User is already suspended"));
-  }
+    if (userToSuspend.status === "suspended") {
+        return next(new BadRequestError("User is already suspended"));
+    }
 
-  // Update the user's status to suspended
-  await User.findByIdAndUpdate(userId, { status: "suspended" });
-  res.json(200).json({ message: "User suspended successfully" });
+    // Update the user's status to suspended
+    await User.findByIdAndUpdate(userId, { status: "suspended" });
+    res.status(200).json({ message: "User suspended successfully" });
 };
 
 export default suspendUser;
